52401328c2429518ee1b193a03e25d0fabb3f061
Peace-Maker <peace-maker@wcfan.de>
2014-08-20 17:07:33 +0200

    Added init scripts for database tables

---
+++ configs/sql-init-scripts/sqlite/smrpg.sql
@@ -0,0 +1,36 @@
+CREATE TABLE players (
+	player_id INTEGER PRIMARY KEY AUTOINCREMENT, 
+	name VARCHAR(64) NOT NULL DEFAULT ' ', 
+	steamid VARCHAR(64) NOT NULL DEFAULT '0' UNIQUE, 
+	level INTEGER DEFAULT '1', 
+	experience INTEGER DEFAULT '0', 
+	credits INTEGER DEFAULT '0', 
+	showmenu INTEGER DEFAULT '1', 
+	fadescreen INTEGER DEFAULT '1', 
+	lastseen INTEGER DEFAULT '0', 
+	lastreset INTEGER DEFAULT '0'
+);
+
+CREATE TABLE upgrades (
+	upgrade_id INTEGER PRIMARY KEY AUTOINCREMENT, 
+	shortname VARCHAR(32) UNIQUE NOT NULL, 
+	date_added INTEGER
+);
+
+CREATE TABLE player_upgrades (
+	player_id INTEGER, 
+	upgrade_id INTEGER, 
+	purchasedlevel INTEGER NOT NULL, 
+	selectedlevel INTEGER NOT NULL, 
+	enabled INTEGER DEFAULT '1', 
+	visuals INTEGER DEFAULT '1', 
+	sounds INTEGER DEFAULT '1', 
+	PRIMARY KEY(player_id, upgrade_id)
+);
+
+CREATE TABLE settings (
+	setting VARCHAR(64) PRIMARY KEY NOT NULL, 
+	value VARCHAR(256) NOT NULL
+);
+
+INSERT INTO settings (setting, value) VALUES ('version', '100');
